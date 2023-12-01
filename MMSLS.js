dc514c7fd3831cfe34bb6dcdb81dff8f6cfe572d
ChengLin <97664970+ziang123@users.noreply.github.com>
2023-12-01 22:27:50 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -1,28 +1,16 @@
-/**
-利用随机概率实现多账户运行
-
-填写格式如下：
-
-
 const MMSLS = [
+    "https://www.maimemo.com/share/page?uid=32529553&pid=fe5003e1b151e64387b8875c72fab5d2&tid=78820a52ed3c233d3ef2f06f5c9d8f56",
+    "https://www.maimemo.com/share/page?uid=32529553&pid=fe5003e1b151e64387b8875c72fab5d2&tid=78820a52ed3c233d3ef2f06f5c9d8f56",
+    "https://www.maimemo.com/share/page?uid=32529553&pid=fe5003e1b151e64387b8875c72fab5d2&tid=78820a52ed3c233d3ef2f06f5c9d8f56",
+    "https://www.maimemo.com/share/page?uid=32529553&pid=fe5003e1b151e64387b8875c72fab5d2&tid=78820a52ed3c233d3ef2f06f5c9d8f56",
+    "https://www.maimemo.com/share/page?uid=32529553&pid=fe5003e1b151e64387b8875c72fab5d2&tid=78820a52ed3c233d3ef2f06f5c9d8f56",
+    "https://www.maimemo.com/share/page?uid=32529553&pid=fe5003e1b151e64387b8875c72fab5d2&tid=78820a52ed3c233d3ef2f06f5c9d8f56",
+    "https://www.maimemo.com/share/page?uid=32529553&pid=fe5003e1b151e64387b8875c72fab5d2&tid=78820a52ed3c233d3ef2f06f5c9d8f56",
+    "https://www.maimemo.com/share/page?uid=32529553&pid=fe5003e1b151e64387b8875c72fab5d2&tid=78820a52ed3c233d3ef2f06f5c9d8f56",
+    "https://www.maimemo.com/share/page?uid=32529553&pid=fe5003e1b151e64387b8875c72fab5d2&tid=78820a52ed3c233d3ef2f06f5c9d8f56",
+    "https://www.maimemo.com/share/page?uid=32529553&pid=fe5003e1b151e64387b8875c72fab5d2&tid=78820a52ed3c233d3ef2f06f5c9d8f56",
+    "https://www.maimemo.com/share/page?uid=32529553&pid=fe5003e1b151e64387b8875c72fab5d2&tid=78820a52ed3c233d3ef2f06f5c9d8f56",
     
-    "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
-    "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
-    "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
-  ]
-=====================以上为范例！=====================
-
-=====================请在下面编辑=====================
-=====================请在下面编辑=====================
-=====================请在下面编辑=====================
-=====================请在下面编辑=====================
-=====================请在下面编辑=====================
- */
-const MMSLS = [
-    
-    "",
-    "",
-    "",
   ]
   /**
    * 生成随机数字
